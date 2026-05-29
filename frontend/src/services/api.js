d93c54a27aae1fdce05a3948253d3d12272f07ba
gourilakshmi97 <gourilakshmigireesh@gmail.com>
2026-05-29 09:35:12 +0530

    update code in api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
   baseURL:
-    "https://backend-405871201173.asia-south1.run.app/api",
+    "https://backend-405871201173.asia-south1.run.app",
 });
 
 API.interceptors.request.use((req) => {
